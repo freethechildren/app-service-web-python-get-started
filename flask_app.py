c53735cee26e1597dd505893a1968aa85d4ecc13
poulad <poulad.pld@gmail.com>
2019-04-09 14:06:06 -0400

    Use App Insights SDK

--- flask_app.py
+++ flask_app.py
@@ -4,20 +4,16 @@ import time
 import requests
 
 from flask import Flask, Response
-
-from applicationinsights.logging import enable
+from applicationinsights.flask.ext import AppInsights
 
 INSTRUMENTATION_KEY = '115fcb01-ff5c-42db-9b69-5e6ae017f9a7'
-# set up logging
-enable(INSTRUMENTATION_KEY)
 
-# log something (this will be sent to the Application Insights service as a trace)
-logging.info('This is a message')
-
-# logging shutdown will cause a flush of all un-sent telemetry items
-logging.shutdown()
-
+# instantiate the Flask application
 app = Flask(__name__)
+app.config['APPINSIGHTS_INSTRUMENTATIONKEY'] = INSTRUMENTATION_KEY
+
+# log requests, traces and exceptions to the Application Insights service
+appinsights = AppInsights(app)
 
 
 @app.route('/')
@@ -28,28 +24,28 @@ def hello_world():
 
 @app.route('/snail')
 def slow():
-    logging.warning('This API is sooooo slow!!!')
+    app.logger.warning('This API is sooooo slow!!!')
     time.sleep(30)
-    logging.info('making an API request to ')
+    app.logger.info('making an API request to ')
     return "I'm ALIVE!"
 
 
 @app.route('/dad-joke')
 def dad_joke():
-    logging.debug('making a request to the Dad Jokes API')
+    app.logger.debug('making a request to the Dad Jokes API')
     response = requests.get('https://icanhazdadjoke.com', headers={"Accept": "text/plain"})
     return response.content
 
 
 @app.route('/500')
 def five_hundred():
-    logging.error('Oops! Something went wrong :)')
+    app.logger.error('Oops! Something went wrong :)')
     return Response(status=500)
 
 
 @app.route('/403')
 def four_oh_three():
-    logging.warning('This guy is not authorized to access. WATCH HIM!')
+    app.logger.warning('This guy is not authorized to access. WATCH HIM!')
     return Response(status=401)
 
 
